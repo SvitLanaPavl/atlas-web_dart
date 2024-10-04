3a625099826eacd93d3bc08d86df1832e2ca9250
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-10-04 13:21:09 -0500

    simplified

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -3,31 +3,20 @@ import '4-util.dart';
 
 Future<num> calculateTotal() async {
   try {
-    String userData = await fetchUserData();
+    Map userData = jsonDecode(await fetchUserData());
 
-    Map<String, dynamic> userMap = jsonDecode(userData);
-    String userId = userMap['id'];
+    String userId = userData['id'];
 
-    String? ordersData = await fetchUserOrders(userId);
-
-    if (ordersData == null) {
-      return -1;
-    }
-
-    List<dynamic> orders = jsonDecode(ordersData);
+    List<dynamic> ordersData = jsonDecode(await fetchUserOrders(userId));
 
     double totalPrice = 0.0;
 
-    for (var product in orders) {
-      String productPriceData = await fetchProductPrice(product);
-
-      double productPrice = (jsonDecode(productPriceData) as num).toDouble();
-      totalPrice += productPrice;
+    for (var product in ordersData) {
+      totalPrice += double.parse(await fetchProductPrice(product));
     }
 
     return totalPrice;
   } catch (error) {
-    print('error caught: $error');
     return -1;
   }
 }
