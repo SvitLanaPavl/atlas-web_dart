b45a7b0ffe225e1e5478cda0eeb84e09c7c240c2
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-10-04 11:42:48 -0500

    checking condition to consider "or" and none in current longest

--- 0x01-dart-function/10-longest-palindrome.dart
+++ 0x01-dart-function/10-longest-palindrome.dart
@@ -4,10 +4,10 @@ String longestPalindrome(String s) {
   String longest = 'none';
 
   for (int i = 0; i < s.length; i++) {
-    for (int j = i + 3; j <= s.length; j++) {
+    for (int j = i + 2; j <= s.length; j++) {
       String substr = s.substring(i, j);
 
-      if (isPalindrome(substr) && substr.length > longest.length) {
+      if (isPalindrome(substr) && (longest == 'none' || substr.length > longest.length)) {
         longest = substr;
       }
     }
