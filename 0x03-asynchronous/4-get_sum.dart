852d01a300f810a1b81a3ef80e88a70fcea46497
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-10-04 12:57:46 -0500

    total price looks wrong checking

---
+++ 0x03-asynchronous/4-get_sum.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+import '4-util.dart';
+
+Future<double> calculateTotal() async {
+  try {
+    String userData = await fetchUserData();
+
+    Map<String, dynamic> userMap = jsonDecode(userData);
+    String userId = userMap['id'];
+
+    String ordersData = await fetchUserOrders(userId);
+
+    List<dynamic> orders = jsonDecode(ordersData);
+
+    double totalPrice = 0.0;
+
+    for (var product in orders) {
+      String productPriceData = await fetchProductPrice(product);
+
+      double productPrice = jsonDecode(productPriceData);
+      totalPrice += productPrice;
+    }
+    return totalPrice;
+  } catch (error) {
+    print('error caught: $error');
+    return -1;
+  }
+}
