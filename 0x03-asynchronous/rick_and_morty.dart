7b7bcc548e5d6dce0b97c6dc1ae4411397e9326b
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-10-04 13:29:13 -0500

    task 5

---
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+Future<void> printRmCharacters() async {
+  const String apiUrl = 'https://rickandmortyapi.com/api/character';
+
+  try {
+    final response = await http.get(Uri.parse(apiUrl));
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+
+      final List<dynamic> characters = data['results'];
+
+      for (var character in characters) {
+        print(character['name']);
+      }
+    } else {
+      print('Failed to load characters. Status code: ${response.statusCode}');
+    }
+  } catch (error) {
+    print('error caught: $error');
+  }
+}
